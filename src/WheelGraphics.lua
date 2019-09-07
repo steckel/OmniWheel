842c938f583ec97ad05d27c257545cf11cce7892
Curtis Steckel <curtis.steckel@gmail.com>
2019-09-06 21:26:37 -0700

    Update design implementation and refactor to extract some constants.

--- src/WheelGraphics.lua
+++ src/WheelGraphics.lua
@@ -1,5 +1,9 @@
 module("WheelGraphics", package.seeall);
 
+local icon_distance_from_center = 132 ;
+local icon_size = 36;
+local text_distance_from_icon = 36;
+
 local NewSector = require("Sector");
 
 WheelGraphics = {}
@@ -8,16 +12,13 @@ WheelGraphics.__index = WheelGraphics;
 function WheelGraphics:New(parent_frame, --[[WheelGeometry ]]wheel_geometry)
   local self = setmetatable({}, WheelViewRenderer);
 
-  -- TODO(steckel): Inject this.
   local ui_scale = parent_frame:GetEffectiveScale();
-  local distance_from_center = 90;
-  local icon_size = 36;
 
   -- Sector 1
   self.sector_one_texture = parent_frame:CreateTexture(nil, "ARTWORK");
   self.sector_one_texture:SetSize(icon_size, icon_size);
   self.sector_one_texture:SetPoint(
-    "CENTER", parent_frame, "CENTER", distance_from_center / ui_scale, 0);
+    "CENTER", parent_frame, "CENTER", icon_distance_from_center / ui_scale, 0);
   self.sector_one_texture:SetTexture("Interface\\ICONS\\INV_Misc_Map_01.blp");
   local sector_one_texture_mask = parent_frame:CreateMaskTexture();
   sector_one_texture_mask:SetTexture("Interface\\CharacterFrame\\TempPortraitAlphaMaskSmall.blp", "CLAMPTOBLACKADDITIVE", "CLAMPTOBLACKADDITIVE")
@@ -26,13 +27,13 @@ function WheelGraphics:New(parent_frame, --[[WheelGeometry ]]wheel_geometry)
   self.sector_one_texture:AddMaskTexture(sector_one_texture_mask);
   self.sector_one_font = parent_frame:CreateFontString(nil, "ARTWORK");
   self.sector_one_font:SetFont("Fonts\\ARIALN.TTF", 14, "THICKOUTLINE");
-  self.sector_one_font:SetPoint("LEFT", self.sector_one_texture, "CENTER", 30, 0);
+  self.sector_one_font:SetPoint("LEFT", self.sector_one_texture, "CENTER", text_distance_from_icon, 0);
   self.sector_one_font:SetText("MAP");
 
   -- Sector 2
   local bisecting_radius_sector_2 = wheel_geometry.sectors[2].sector:GetBisectingRadius();
   local sector_two_x, sector_two_y =
-    polar_to_cartesian(bisecting_radius_sector_2, distance_from_center);
+    polar_to_cartesian(bisecting_radius_sector_2, icon_distance_from_center);
   self.sector_two_texture = parent_frame:CreateTexture(nil, "ARTWORK");
   self.sector_two_texture:SetSize(icon_size, icon_size);
   self.sector_two_texture:SetPoint(
@@ -45,13 +46,13 @@ function WheelGraphics:New(parent_frame, --[[WheelGeometry ]]wheel_geometry)
   self.sector_two_texture:AddMaskTexture(sector_two_texture_mask);
   self.sector_two_font = parent_frame:CreateFontString(nil, "ARTWORK");
   self.sector_two_font:SetFont("Fonts\\ARIALN.TTF", 14, "THICKOUTLINE");
-  self.sector_two_font:SetPoint("BOTTOMLEFT", self.sector_two_texture, "CENTER", 0, 30);
+  self.sector_two_font:SetPoint("BOTTOMLEFT", self.sector_two_texture, "CENTER", 0, text_distance_from_icon);
   self.sector_two_font:SetText("SPELL BOOK");
 
   -- Sector 3
   local bisecting_radius_sector_3 = wheel_geometry.sectors[3].sector:GetBisectingRadius();
   local sector_three_x, sector_three_y =
-    polar_to_cartesian(bisecting_radius_sector_3, distance_from_center);
+    polar_to_cartesian(bisecting_radius_sector_3, icon_distance_from_center);
   self.sector_three_texture = parent_frame:CreateTexture(nil, "ARTWORK");
   self.sector_three_texture:SetSize(icon_size, icon_size);
   self.sector_three_texture:SetPoint(
@@ -64,13 +65,13 @@ function WheelGraphics:New(parent_frame, --[[WheelGeometry ]]wheel_geometry)
   self.sector_three_texture:AddMaskTexture(sector_three_texture_mask);
   self.sector_three_font = parent_frame:CreateFontString(nil, "ARTWORK");
   self.sector_three_font:SetFont("Fonts\\ARIALN.TTF", 14, "THICKOUTLINE");
-  self.sector_three_font:SetPoint("BOTTOM", self.sector_three_texture, "CENTER", 0, 30);
+  self.sector_three_font:SetPoint("BOTTOM", self.sector_three_texture, "CENTER", 0, text_distance_from_icon);
   self.sector_three_font:SetText("QUEST LOG");
 
   -- Sector 4
   local bisecting_radius_sector_4 = wheel_geometry.sectors[4].sector:GetBisectingRadius();
   local sector_four_x, sector_four_y =
-    polar_to_cartesian(bisecting_radius_sector_4, distance_from_center);
+    polar_to_cartesian(bisecting_radius_sector_4, icon_distance_from_center);
   self.sector_four_texture = parent_frame:CreateTexture(nil, "ARTWORK");
   self.sector_four_texture:SetSize(icon_size, icon_size);
   self.sector_four_texture:SetPoint(
@@ -83,23 +84,23 @@ function WheelGraphics:New(parent_frame, --[[WheelGeometry ]]wheel_geometry)
   self.sector_four_texture:AddMaskTexture(sector_four_texture_mask);
   self.sector_four_font = parent_frame:CreateFontString(nil, "ARTWORK");
   self.sector_four_font:SetFont("Fonts\\ARIALN.TTF", 14, "THICKOUTLINE");
-  self.sector_four_font:SetPoint("BOTTOMRIGHT", self.sector_four_texture, "CENTER", 0, 30);
+  self.sector_four_font:SetPoint("BOTTOMRIGHT", self.sector_four_texture, "CENTER", 0, text_distance_from_icon);
   self.sector_four_font:SetText("TALENTS");
 
   -- Sector 5
   self.sector_five_texture = parent_frame:CreateTexture(nil, "ARTWORK");
   self.sector_five_texture:SetSize(32, 32);
-  self.sector_five_texture:SetPoint("CENTER", parent_frame, "CENTER", -distance_from_center / ui_scale, 0);
+  self.sector_five_texture:SetPoint("CENTER", parent_frame, "CENTER", -icon_distance_from_center / ui_scale, 0);
   SetPortraitTexture(self.sector_five_texture, "player");
   self.sector_five_font = parent_frame:CreateFontString(nil, "ARTWORK");
   self.sector_five_font:SetFont("Fonts\\ARIALN.TTF", 14, "THICKOUTLINE");
-  self.sector_five_font:SetPoint("RIGHT", self.sector_five_texture, "CENTER", -30, 0);
+  self.sector_five_font:SetPoint("RIGHT", self.sector_five_texture, "CENTER", -1 * text_distance_from_icon, 0);
   self.sector_five_font:SetText("CHARACTER");
 
   -- Sector 6
   local bisecting_radius_sector_6 = wheel_geometry.sectors[6].sector:GetBisectingRadius();
   local sector_six_x, sector_six_y =
-    polar_to_cartesian(bisecting_radius_sector_6, distance_from_center);
+    polar_to_cartesian(bisecting_radius_sector_6, icon_distance_from_center);
   self.sector_six_texture = parent_frame:CreateTexture(nil, "ARTWORK");
   self.sector_six_texture:SetSize(icon_size, icon_size);
   self.sector_six_texture:SetPoint(
@@ -112,13 +113,13 @@ function WheelGraphics:New(parent_frame, --[[WheelGeometry ]]wheel_geometry)
   self.sector_six_texture:AddMaskTexture(sector_six_texture_mask);
   self.sector_six_font = parent_frame:CreateFontString(nil, "ARTWORK");
   self.sector_six_font:SetFont("Fonts\\ARIALN.TTF", 14, "THICKOUTLINE");
-  self.sector_six_font:SetPoint("TOPRIGHT", self.sector_six_texture, "CENTER", 0, -30);
+  self.sector_six_font:SetPoint("TOPRIGHT", self.sector_six_texture, "CENTER", 0, -1 * text_distance_from_icon);
   self.sector_six_font:SetText("SOCIAL");
 
   -- Sector 7
   local bisecting_radius_sector_7 = wheel_geometry.sectors[7].sector:GetBisectingRadius();
   local sector_seven_x, sector_seven_y =
-    polar_to_cartesian(bisecting_radius_sector_7, distance_from_center);
+    polar_to_cartesian(bisecting_radius_sector_7, icon_distance_from_center);
   self.sector_seven_texture = parent_frame:CreateTexture(nil, "ARTWORK");
   self.sector_seven_texture:SetSize(icon_size, icon_size);
   self.sector_seven_texture:SetPoint(
@@ -131,13 +132,13 @@ function WheelGraphics:New(parent_frame, --[[WheelGeometry ]]wheel_geometry)
   self.sector_seven_texture:AddMaskTexture(sector_seven_texture_mask);
   self.sector_seven_font = parent_frame:CreateFontString(nil, "ARTWORK");
   self.sector_seven_font:SetFont("Fonts\\ARIALN.TTF", 14, "THICKOUTLINE");
-  self.sector_seven_font:SetPoint("TOP", self.sector_seven_texture, "CENTER", 0, -30);
+  self.sector_seven_font:SetPoint("TOP", self.sector_seven_texture, "CENTER", 0, -1 * text_distance_from_icon);
   self.sector_seven_font:SetText("ALL BAGS");
 
   -- Sector 8
   local bisecting_radius_sector_8 = wheel_geometry.sectors[8].sector:GetBisectingRadius();
   local sector_eight_x, sector_eight_y =
-    polar_to_cartesian(bisecting_radius_sector_8, distance_from_center);
+    polar_to_cartesian(bisecting_radius_sector_8, icon_distance_from_center);
   self.sector_eight_texture = parent_frame:CreateTexture(nil, "ARTWORK");
   self.sector_eight_texture:SetSize(icon_size, icon_size);
   self.sector_eight_texture:SetPoint(
